2ad23f18057c895902caec30b2568d41a4f155f2
avrudik <avrudik@kaf65.ru>
2021-08-29 22:33:28 +0300

    added 4 task

--- course_tasks/and/hmw_1/task_4.py
+++ course_tasks/and/hmw_1/task_4.py
@@ -1,1 +1,11 @@
--
+a = input('Input smth and the program will return the type of it (except for iterable objects): ')
+c = a.isdigit()
+if c:
+    print('int')
+else:
+    try:
+        b = float(a)
+        print('float')
+    except ValueError:
+        print('str')
+
